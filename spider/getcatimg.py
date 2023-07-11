c907332191952537c77a37d04ffd20096c04fc96
kelvin <kelvinlinkk@outlook.com>
2023-07-11 15:03:35 +0800

    get cat image

---
+++ spider/getcatimg.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import os
+
+url = 'https://api.thecatapi.com/v1/images/search'
+mylist = []
+num = int(input("請輸入圖片數量"))
+for i in range(num):
+    res = requests.get(url)
+    mylist.append(res.json()[0])
+    imgURL = res.json()[0]['url']
+    os.system('wget ' + imgURL + ' -P ~/桌面/ncku/spider/cats/')
+
+with open('result.json', 'w', encoding='utf-8') as f:
+    f.write("{\n")
+    for i in range(num):
+        f.write('\"pic' + str(i+1) + '\":')
+        json.dump(mylist[i], f, indent=2, sort_keys=True, ensure_ascii=False)
+        if i != num-1:
+            f.write(',\n')
+    f.write("}")
+'''
+with open('cat.json','w',encoding='utf-8') as f:
+    f.write("{")
+    for i in range(num):
+        f.write('\"pic' + str(i+1) + '\":')
+        f.write('\"' + imgURL + '\"')
+        if i != num-1:
+            f.write(',')
+    f.write('}')
+'''
---
+++ spider/myjson.py
@@ -0,0 +1,18 @@
+import requests
+import json
+#先將欲發出 GET 請求的網址先存在 url
+url = 'http://dev.vincent55.tw/json' 
+#對 url 發出 GET 請求，並將 Response 物件透過 json 解析後存在 res
+res = requests.get(url).json()
+print(type(res), res)
+print(res['slideshow']['slides'][1]['title'])
+ 
+# 將回傳值存於 result.json
+with open('result.json', 'w', encoding='utf-8') as f:
+    json.dump(res, f, indent=2, sort_keys=True, ensure_ascii=False)
+ 
+'''
+<class 'dict'> {'slideshow': {'author': 'Yours Truly', 'date': 'date of publication', 'slides': [{'title': 'Wake up to WonderWidgets!', 'type': 'all'}, {'items': ['Why <em>WonderWidgets</em> are great', 'Who <em>buys</em> WonderWidgets'], 'title': 'Overview', 'type': 'all'}], 'title': 'Sample Slide Show'}}
+Overview
+'''
+
