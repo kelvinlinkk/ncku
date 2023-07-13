61d4d3665a2db7dd563848b4301ebb3a7ed9f614
kelvin <kelvinlinkk@outlook.com>
2023-07-13 16:51:50 +0800

    test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import random
 print("早安 ",end=" 啾咪")
 mylist = [97,77.7,89.3,0,100]
 print(sum(mylist))
@@ -15,3 +16,6 @@ a[0].append(1)
 print(len(a))
 '''
 print([x for x in range(-3)])
+
+print(random.choice([0,1,2,3]))
+print([0,1,3,2].index(max([0,1,2])))
