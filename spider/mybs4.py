083abb1e9c418e18dc9b4f29e4a68c27c445aa94
Kun-Yi Lin <kelvinlinkk@outlook.com>
2023-07-19 22:56:28 +0800

    電腦自動同步:update

--- spider/mybs4.py
+++ spider/mybs4.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-url = 'http://127.0.0.1:3000/home.html'
+url = 'https://kelvinlinkk.github.io/home.html'
 txt = requests.get(url).text
 soup = BeautifulSoup(txt,'html.parser')
 # print(soup.prettify())
